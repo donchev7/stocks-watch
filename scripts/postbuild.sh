33e9e9c3c9c0a70bc73982d4fb76b16af4a9b51e
Bobby Donchev <contact@donchev.is>
2022-03-03 19:32:19 +0100

    add deployment scripts

---
+++ scripts/postbuild.sh
@@ -0,0 +1,6 @@
+set -ex
+
+find dist/handlers -type f -name '*.js' -exec sed -i '' -e 's/..\/..\/adapters/..\/adapters/g' {} +
+find dist/handlers -type f -name '*.js' -exec sed -i '' -e 's/..\/..\/config/..\/config/g' {} +
+find dist/handlers -type f -name '*.js' -exec sed -i '' -e 's/..\/..\/entities/..\/entities/g' {} +
+find dist/handlers -type f -name '*.js' -exec sed -i '' -e 's/..\/..\/logger/..\/logger/g' {} +
