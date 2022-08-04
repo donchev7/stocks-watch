afa7f0cc951d6c9fe1e566d5475eee496ccd35fc
Bobby Donchev <contact@donchev.is>
2022-08-04 17:02:09 +0200

    add portfolio change calculation

--- src/handlers/get-portfolio/handler.ts
+++ src/handlers/get-portfolio/handler.ts
@@ -8,8 +8,9 @@ const portfolioResponse = z.object({
   resource: z.object({
     name: z.string(),
     createdAt: z.date(),
-    assets: z.array(assetSchema.omit(metaFields)).optional()
-  })
+    change: z.string(),
+    assets: z.array(assetSchema.omit(metaFields).extend({ change: z.string() })).optional(),
+  }),
 })
 
 const portfolioName = z
@@ -22,21 +23,47 @@ interface DB {
   getAssets(log: Logger, portfolioName: string): Promise<Asset[]>
 }
 
+const isPositive = (asset: Asset) => asset.lastPriceCheckValue > asset.investmentValue
+
+const calculateChange = (asset: Asset) => {
+  const minValue = Math.min(asset.lastPriceCheckValue ?? 0, asset.investmentValue)
+  const maxValue = Math.max(asset.lastPriceCheckValue ?? 0, asset.investmentValue)
+  const result = (1 - minValue / maxValue) * 100
+
+  return isPositive(asset) ? result : -result
+}
+
+const getChange = (asset: Asset) => {
+  const value = calculateChange(asset)
+
+  return `${value.toFixed(2)}%`
+}
+
 export const newHandler = (db: DB) => handler(db)
 
 const handler = function (db: DB) {
   return async (context: Context, _: HttpRequest) => {
     const nameInput = context.bindingData['name']
     const pName = await portfolioName.parseAsync(nameInput)
+    let portfolioPerformance = 0
 
     const entity = await db.getPortfolio(context.log, pName)
     const entityAssets = await db.getAssets(context.log, pName)
+    const assets = entityAssets.map((asset) => {
+      portfolioPerformance += calculateChange(asset)
 
-    const resource = { ...entity, assets: entityAssets }
+      return {
+        ...asset,
+        change: getChange(asset),
+      }
+    })
+
+    const portfolioChange = `${portfolioPerformance.toFixed(2)}%`
+    const resource = { ...entity, change: portfolioChange, assets }
 
     return {
       status: 200,
-      body: portfolioResponse.parse({ resource })
+      body: portfolioResponse.parse({ resource }),
     }
   }
 }
